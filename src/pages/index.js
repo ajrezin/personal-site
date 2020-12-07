0b73a697b504b7f6cbe539f130a5f970c0ad23f7
arielrezinn <arielrezin@gmail.com>
2020-12-06 23:10:56 -0600

    pronouns and cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,15 +17,20 @@ const IndexPage = () => (
         width: ['90%', '50%', '50%'],
       }}>
       <h2>Hi, I'm Ariel!</h2>
+      <h6>
+        <Styled.a href="https://pronoun.is/she/her">pronoun.is/she</Styled.a>
+        &nbsp;or&nbsp;
+        <Styled.a href="https://pronoun.is/they/them">pronoun.is/they</Styled.a>
+      </h6>
       <p role='region' style={{ textAlign: 'center' }}>
-        I'm a software developer who wants to make the world a better place through my work. I currently &nbsp;
+        I'm a software developer who wants to make the world a better place. I currently&nbsp;
         <Styled.a target="_blank" href="https://resume.arielrezin.com">work <MdOpenInNew aria-label="Opens Ariel's resume in a new window" size={12} /></Styled.a>
-        &nbsp; as a dev at &nbsp;
+        &nbsp;as a developer at&nbsp;
         <Styled.a href="https://sentry.com">Sentry Inc.<MdOpenInNew aria-label="Opens Sentry's website in a new window" size={12} /></Styled.a>
-        &nbsp; and study Computer Science, Disability Rights & Services, and German at the University of Wisconsin-Madison.
-        When I'm not busy hammocking or fostering dogs, I spend my time going down rabbit holes while working on &nbsp;
+        &nbsp;and study Computer Science, Disability Rights & Services, and German at the University of Wisconsin-Madison.
+        When I'm not busy hammocking or fostering dogs, I spend my time going down rabbit holes while working on&nbsp;
         <Styled.a href="https://github.com/arielrezinn">personal projects <MdOpenInNew aria-label="Opens Ariel's Github in a new window" size={12} /></Styled.a>.
-        If you'd like to get in touch, you can find me on &nbsp;
+        If you'd like to get in touch, you can find me on&nbsp;
         <Styled.a href="https://linkedin.com/in/arielrezin">LinkedIn <MdOpenInNew aria-label="Opens Ariel's profile in a new window" size={12} /></Styled.a>.
       </p>
     </Flex>
