974d4c4fbd9e5660d2150726a58fee367828715e
Ariel Rezin <arielrezin@gmail.com>
2022-11-02 21:18:17 -0500

    fix contrast issue

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -4,7 +4,7 @@ export default {
     colors: {
         text: '#455300',
         background: '#f5f2e4',
-        primary: '#ba7d04',
+        primary: '#a25c01',
         secondary: '#7a6ac0',
         muted: '#e9e1c9',
         modes: {
